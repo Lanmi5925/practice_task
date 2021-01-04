eade9861065a72818f6291197f232c9cfd0a5243
Lanmi5925 <32831183+Lanmi5925@users.noreply.github.com>
2021-01-04 23:57:20 +0800

    第一题

---
+++ 01-Fibonacci_sequence.py
@@ -0,0 +1,34 @@
+# 上楼梯，共N阶楼梯。可以一次上一阶，也可以一次上两阶，上到第N阶有多少种方法
+# 输入一个数字打印出“共xx种方法”和“程序用时xx秒”
+import time
+
+def stairs(n, cache=None):
+	if cache is None:
+		cache = {}
+	if n in cache:
+		return cache[n]
+	if n == 1:
+		cache[n] = 1
+		return 1
+	if n == 2:
+		cache[n] = 2
+		return 2
+	if n >= 3:
+		cache[n] = stairs(n-1, cache) + stairs(n-2, cache)
+		return cache[n]
+
+def test(n):
+	print("台阶数：%d阶"%n)
+	a = time.time()
+	f = stairs(n)
+	b = time.time()
+	c = b - a
+	print("共有%d种方法上楼"%f)
+	print("解题耗时%f秒"%c)
+
+if __name__ == '__main__':
+	s = '''上楼梯，共N阶楼梯。可以一次上一阶，也可以一次上两阶，上到第N阶有多少种方法'''
+	print(s)
+	for i in range(50):
+		test(i+1)
+
